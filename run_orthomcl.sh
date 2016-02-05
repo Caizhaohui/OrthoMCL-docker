0b4bb501ce6928145109d1b4494da297b51520ce
Josh Granek <josh@duke.edu>
2016-02-05 12:54:36 -0500

    Removed the automatic deletion of the orthomcl-mysql and orthomcl-run containers.  Added instructions for cleaning up or resuming work in the container.

--- run_orthomcl.sh
+++ run_orthomcl.sh
@@ -7,14 +7,21 @@ docker run --name orthomcl-mysql -e MYSQL_ROOT_PASSWORD=asdf1234 -e MYSQL_USER=o
 # Only keep container id in temporary file
 echo `tail -n 1 .ortho_mcl_db_container_id` > .ortho_mcl_db_container_id
 
-sleep 10
+sleep 2
 
 # Start OrthoMCL container
 echo Starting OrthoMCL container...
 echo Mounting local directory ${RUN_DIR} to /host_dir within the OrthoMCL container
-docker run -it --rm --name orthomcl-run --link orthomcl-mysql:mysql -v ${RUN_DIR}:/host_dir granek/orthomcl /bin/bash
+docker run -it --name orthomcl-run --link orthomcl-mysql:mysql -v ${RUN_DIR}:/host_dir granek/orthomcl /bin/bash
 
 # Clean up after MySQL container
-docker kill `cat .ortho_mcl_db_container_id`
-docker rm `cat .ortho_mcl_db_container_id`
-rm .ortho_mcl_db_container_id
+# docker kill `cat .ortho_mcl_db_container_id`
+# docker rm `cat .ortho_mcl_db_container_id`
+# rm .ortho_mcl_db_container_id
+echo "Leaving MySQL container and OrthoMCL container intact"
+echo ""
+echo "To clean up containers run:"
+echo "docker stop orthomcl-mysql; docker rm orthomcl-run orthomcl-mysql"
+echo ""
+echo "To continue working in the OrthoMCL container run:"
+echo "docker start orthomcl-run; docker exec -ti orthomcl-run bash"
